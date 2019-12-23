744476edbe1e9e2e11824b849e97dd54098f2d59
Jon Wålstedt <jon.walstedt@svt.se>
2019-12-23 12:41:07 +0100

    Refactored keymapping logic based on spacebarracecars setup

--- keyboards/v60_type_r/keymaps/v60_r_custom/config.h
+++ keyboards/v60_type_r/keymaps/v60_r_custom/config.h
@@ -16,3 +16,177 @@
 #pragma once
 
 // place overrides here
+
+// Normal first shift second
+#define NORM_SHIFT(kc1, kc2) \
+if (record->event.pressed) { \
+  if (lshift || rshift) { \
+    register_code(KC_LSFT); \
+    unregister_code(kc2); \
+    register_code(kc2); \
+    add_to_prev(kc2); \
+  } else { \
+    unregister_code(KC_LSFT); \
+    unregister_code(kc1); \
+    register_code(kc1); \
+  } \
+} else { \
+  unregister_code(kc1); \
+  unregister_code(kc2); \
+} \
+return false;
+
+// Shift first normal second
+#define SWITCH_SHIFT(kc1, kc2) \
+if (record->event.pressed) { \
+  if (lshift || rshift) { \
+    unregister_code(KC_LSFT); \
+    unregister_code(kc2); \
+    register_code(kc2); \
+    add_to_prev(kc2); \
+  } else { \
+    register_code(KC_LSFT); \
+    unregister_code(kc1); \
+    register_code(kc1); \
+    add_to_prev(kc1); \
+  } \
+} else { \
+  unregister_code(kc1); \
+  unregister_code(kc2); \
+  unreg_prev(); \
+  if (lshift || rshift) \
+    register_code(KC_LSFT); \
+  else \
+    unregister_code(KC_LSFT); \
+} \
+return false;
+
+// Shift both
+#define SHIFT_SHIFT(kc1, kc2) \
+if (record->event.pressed) { \
+  register_code(KC_LSFT); \
+  if (lshift || rshift) { \
+    unregister_code(kc2); \
+    register_code(kc2); \
+    add_to_prev(kc2); \
+  } else { \
+    unregister_code(kc1); \
+    register_code(kc1); \
+    add_to_prev(kc1); \
+  } \
+} else { \
+  unregister_code(kc1); \
+  unregister_code(kc2); \
+  unreg_prev(); \
+  if (lshift || rshift) \
+    register_code(KC_LSFT); \
+  else \
+    unregister_code(KC_LSFT); \
+} \
+return false;
+
+// Never shifted
+#define NORM_NORM(kc1, kc2) \
+if (record->event.pressed) { \
+  unregister_code(KC_LSFT); \
+  if (lshift || rshift) { \
+    unregister_code(kc2); \
+    register_code(kc2); \
+    add_to_prev(kc2); \
+  } else { \
+    unregister_code(kc1); \
+    register_code(kc1); \
+  } \
+} else { \
+  unregister_code(kc1); \
+  unregister_code(kc2); \
+  unreg_prev(); \
+  if (lshift || rshift) \
+    register_code(KC_LSFT); \
+  else \
+    unregister_code(KC_LSFT); \
+} \
+return false;
+
+// Normal first altgr second
+#define NORM_ALGR(kc1, kc2) \
+if (record->event.pressed) { \
+  unregister_code(KC_LSFT); \
+  if (lshift || rshift) { \
+    register_code(KC_RALT); \
+    unregister_code(kc2); \
+    register_code(kc2); \
+    unregister_code(kc2); \
+    register_code(KC_LSFT); \
+    unregister_code(KC_RALT); \
+  } else { \
+    unregister_code(kc1); \
+    register_code(kc1); \
+    unregister_code(kc1); \
+  } \
+} \
+return false;
+
+// Shift first, altgr shift second
+#define SHIFT_SHIFT_ALGR(kc1, kc2) \
+if (record->event.pressed) { \
+  unregister_code(KC_LSFT); \
+  if (lshift || rshift) { \
+    register_code(KC_RALT); \
+    unregister_code(kc2); \
+    register_code(kc2); \
+    unregister_code(kc2); \
+    register_code(KC_LSFT); \
+    unregister_code(KC_RALT); \
+  } else { \
+    register_code(KC_LSFT); \
+    unregister_code(kc1); \
+    register_code(kc1); \
+    unregister_code(kc1); \
+    unregister_code(KC_LSFT); \
+  } \
+} \
+return false;
+
+// Shift altgr first shift second
+#define SHIFT_ALGR_ALGR(kc1, kc2) \
+if (record->event.pressed) { \
+  unregister_code(KC_LSFT); \
+  if (lshift || rshift) { \
+    register_code(KC_RALT); \
+    unregister_code(kc2); \
+    register_code(kc2); \
+    unregister_code(kc2); \
+    unregister_code(KC_RALT); \
+  } else { \
+    register_code(KC_LSFT); \
+    register_code(KC_RALT); \
+    unregister_code(kc1); \
+    register_code(kc1); \
+    unregister_code(kc1); \
+    unregister_code(KC_RALT); \
+    if (lshift || rshift) \
+      register_code(KC_LSFT); \
+    else \
+      unregister_code(KC_LSFT); \
+    } \
+} \
+return false;
+
+// Different keycode when Ctrl is pressed
+#define CTRL(kc1, kc2) \
+if(record->event.pressed) { \
+  if (lshift || rshift) \
+    register_code(KC_LSFT); \
+  else \
+    unregister_code(KC_LSFT); \
+  if (keyboard_report->mods & (MOD_BIT(KC_LCTL) | MOD_BIT(KC_RCTL))){ \
+    register_code(kc2); \
+  } else { \
+    register_code(kc1); \
+  } \
+} else { \
+    unregister_code(kc1); \
+    unregister_code(kc2); \
+} \
+return false;
--- keyboards/v60_type_r/keymaps/v60_r_custom/keymap.c
+++ keyboards/v60_type_r/keymaps/v60_r_custom/keymap.c
@@ -1,11 +1,6 @@
 #include QMK_KEYBOARD_H
-
-// enum custom_keycodes {
-//     KC_DTQS = SAFE_RANGE,
-//     KC_CMEX,
-//     G_START,
-//     G_EXIT
-// };
+#include "keymap_swedish.h"
+#include <print.h>
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap 0: Default Layer (Qwerty also showing with shift modifier)
@@ -22,27 +17,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [0] = LAYOUT_60_iso(
-
-    KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQL,       KC_BSPC,
-    KC_TAB,      KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC,           KC_RBRC,
-    MO(2),      KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_COMM, KC_QUOT, KC_NUBS, KC_ENT,
-    KC_LSFT,   KC_COMM, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_MINS,     KC_RSFT,
+    KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, SE_PLUS, SE_ACUT,       KC_BSPC,
+    KC_TAB,      KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, SE_AA, SE_QUOT,
+    MO(2),      KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, SE_OSLH, SE_AE, SE_LESS,  KC_ENT,
+    KC_LSFT,   KC_COMM, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,     KC_RSFT,
     KC_LCTL, KC_LALT, KC_LGUI,                KC_SPC,                  MO(1), KC_RALT, KC_APP, KC_RCTL),
 
-  /* Keymap 1: Shifted Layer
-   * ,-------------------------------------------------------------------------------------------------.
-   * |     |  !   |  @   |  #   |  $   |  %   |  ^   |  &   |  *   |  (   |  )   |  _   |  +   |       |
-   * |-------------------------------------------------------------------------------------------------|
-   * |        Q   |  W   |  E   |  R   |  T   |  Y   |  U   |  I   |  O   |  P   |  {   |  }   |       |
-   * |---------------------------------------------------------------------------------------------|   |
-   * |         |  A   |  S   |  D   |  F   |  G   |  H   |  J   |  K   |  L   |  :   |  "   |  |   |   |
-   * |-------------------------------------------------------------------------------------------------|
-   * |        |  ~  |  Z   |  X   |  C   |  V   |  B   |  N   |  M   |  ;   |  :   |  ?   |            |
-   * |-------------------------------------------------------------------------------------------------|
-   * |       |       |       |                                          |       |       |      |       |
-   * `-------------------------------------------------------------------------------------------------'
-   */
-  /*[0] = LAYOUT_60_iso(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL, KC_EQL, KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, MO(2), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_NUBS, KC_ENT, KC_LSFT, KC_COMM, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_MINS, KC_RSFT, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, MO(1), KC_RALT, KC_APP, KC_RCTL),*/
 
     /* Keymap 1: Programming Layer
    * ,-------------------------------------------------------------------------------------------------.
@@ -58,7 +38,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
 
-  [1] = LAYOUT_60_iso(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_TRNS, RSFT(KC_2), RSFT(KC_5), RSFT(KC_EQL), RSFT(KC_3), RSFT(KC_BSLASH), RSFT(KC_RBRACKET), RSFT(KC_SLASH), KC_SLASH, RALT(KC_4), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_RPRN, KC_ASTR, RALT(KC_8), RSFT(RALT(KC_8)), RSFT(RALT(KC_9)), RALT(KC_9), RSFT(KC_9), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RALT(KC_RBRACKET), KC_SLASH, KC_TILD, KC_TILD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+  [1] = LAYOUT_60_iso(
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL,
+    KC_TRNS, RSFT(KC_2), RSFT(KC_5), RSFT(KC_EQL), RSFT(KC_3), RSFT(KC_BSLASH), RSFT(KC_RBRACKET), RSFT(KC_SLASH), KC_MINS, RALT(KC_4), KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_MINS, KC_RPRN, KC_ASTR, RALT(KC_8), RSFT(RALT(KC_8)), RSFT(RALT(KC_9)), RALT(KC_9), RSFT(KC_9),
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RALT(KC_RBRACKET), KC_SLASH, KC_TILD, KC_TILD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
 
 
@@ -80,291 +65,132 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 
-// SWE to EN KC map
-
-// KC_ENTER KC_ENT
-// KC_ESCAPE KC_ESC
-// KC_BSPACE KC_BSPC
-// KC_TAB
-// KC_SPACE KC_SPC
-// KC_MINUS KC_MINS S(KC_MINUS) = +
-// KC_EQUAL KC_EQL
-// KC_LBRACKET KC_LBRC
-// KC_RBRACKET KC_RBRC
-// KC_BSLASH KC_BSLS
-// KC_NONUS_HASH KC_NUHS
-// KC_SCOLON KC_SCLN = ö
-// KC_QUOTE KC_QUOT
-// KC_GRAVE KC_GRV
-// KC_ZKHK
-// KC_COMMA KC_COMM = ,
-// KC_DOT
-// KC_SLASH KC_SLSH
-// KC_NONUS_BSLASH
-static bool shouldReaddShiftOnRelease = false;
-
-
-// static bool shiftIsRegistered = false;
-//
-// static void registerShift(void) {
-//   shiftIsRegistered = true;
-//   register_code(KC_RSFT);
-// }
-//
-// static void unRegisterShift(void) {
-//   shiftIsRegistered = true;
-//   unregister_code(KC_RSFT);
-// }
-
-static bool altIsRegistered = false;
-
-static void registerAlt(void) {
-  altIsRegistered = true;
-  register_code(KC_RALT);
+// TODO REMOVE DEBUG LATER
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=false;
+  debug_keyboard=true;
+  //debug_mouse=true;
 }
 
-static void unRegisterAlt(void) {
-  altIsRegistered = false;
-  unregister_code(KC_RALT);
-}
+// Number of items that are saved in prev_kcs
+uint8_t prev_indx = 0;
+// Used to save the last 6 actual keycodes activated by frankenkeycodes
+uint16_t prev_kcs[6] = {0, 0, 0, 0, 0, 0};
 
-// static int MY_SEMICOL = KC_COMM;
-// static int MY_COL = S(KC_DOT);
-static int MY_DASH = S(KC_SLSH);
-static int MY_EQL = KC_0;
-static int MY_PLUS = KC_MINS;
-
-bool topRow(uint16_t keycode, keyrecord_t *record) {
-  bool isLeftShiftPressed = get_mods() & MOD_BIT(KC_LSHIFT);
-  bool isRightShiftPressed = get_mods() & MOD_BIT(KC_RSHIFT);
-
-  switch (keycode) {
-    // Shift 2 to @
-    case KC_2:
-      if (record->event.pressed){
-        if (isLeftShiftPressed || isRightShiftPressed) {
-          unregister_code(KC_LSHIFT);
-          unregister_code(KC_RSHIFT);
-          registerAlt();
-          register_code(KC_2);
-        } else {
-          register_code(KC_2);
-        }
-      } else {
-        unregister_code(KC_2);
-      }
-      return false;
-
-    // Shift 4 to $
-    case KC_4:
-      if (record->event.pressed){
-        if (isLeftShiftPressed || isRightShiftPressed) {
-          unregister_code(KC_LSHIFT);
-          unregister_code(KC_RSHIFT);
-          registerAlt();
-          register_code(KC_4);
-        } else {
-          register_code(KC_4);
-        }
-      } else {
-        unregister_code(KC_4);
-      }
-      return false;
-
-    // Shift 6 to ^
-    case KC_6:
-      if (record->event.pressed){
-        if (isLeftShiftPressed || isRightShiftPressed) {
-          unregister_code(KC_6);
-          register_code(KC_RBRACKET);
-        } else {
-          register_code(KC_6);
-        }
-      } else {
-        unregister_code(KC_6);
-        unregister_code(KC_RBRACKET);
-      }
-      return false;
-
-    // Shift 7 to &
-    case KC_7:
-      if (record->event.pressed){
-        if (isLeftShiftPressed || isRightShiftPressed) {
-          unregister_code(KC_7);
-          register_code(KC_6);
-        } else {
-          register_code(KC_7);
-        }
-      } else {
-        unregister_code(KC_7);
-        unregister_code(KC_6);
-      }
-      return false;
-
-    // Shift 8 to *
-    case KC_8:
-      if (record->event.pressed){
-        if (isLeftShiftPressed || isRightShiftPressed) {
-          unregister_code(KC_8);
-          register_code(KC_NONUS_HASH);
-        } else {
-          register_code(KC_8);
-        }
-      } else {
-        unregister_code(KC_8);
-        unregister_code(KC_NONUS_HASH);
-      }
-      return false;
-
-    // Shift 9 to (
-    case KC_9:
-      if (record->event.pressed){
-        if (isLeftShiftPressed || isRightShiftPressed) {
-          unregister_code(KC_9);
-          register_code(KC_8);
-        } else {
-          register_code(KC_9);
-        }
-      } else {
-        unregister_code(KC_9);
-        unregister_code(KC_8);
-      }
-      return false;
-
-    // Shift 9 to (
-    case KC_0:
-      if (record->event.pressed){
-        if (isLeftShiftPressed || isRightShiftPressed) {
-          unregister_code(KC_0);
-          register_code(KC_9);
-        } else {
-          register_code(KC_0);
-        }
-      } else {
-        unregister_code(KC_0);
-        unregister_code(KC_9);
-      }
-      return false;
-
-    // + to -
-    // ? to _
-    case KC_MINS:
-      if (record->event.pressed){
-        if (isLeftShiftPressed || isRightShiftPressed) {
-          unregister_code(KC_MINS);
-          register_code(MY_DASH);
-        } else {
-          register_code(KC_SLSH);
-        }
-      } else {
-        unregister_code(KC_SLSH);
-        unregister_code(KC_MINS);
-        unregister_code(MY_DASH);
-      }
-      return false;
-
-    // ´ to +
-    // ` to =
-    case KC_EQL:
-      if (record->event.pressed){
-        if (isLeftShiftPressed || isRightShiftPressed) {
-          unregister_code(KC_EQL);
-          unregister_code(KC_RSFT);
-          unregister_code(KC_LSFT);
-          register_code(MY_PLUS);
-          shouldReaddShiftOnRelease = true;
-        } else {
-          register_code(KC_RSFT);
-          register_code(MY_EQL);
-        }
-      } else {
-        unregister_code(KC_RSFT);
-        unregister_code(MY_PLUS);
-        unregister_code(MY_EQL);
-        if (shouldReaddShiftOnRelease) {
-          register_code(KC_RSFT);
-        }
-      }
-      return false;
-
-    default:
-      if (altIsRegistered) {
-        unRegisterAlt();
-      }
-      if (shouldReaddShiftOnRelease) {
-        unregister_code(KC_RSFT);
-        shouldReaddShiftOnRelease = false;
-      }
-      return true;
+/*
+Used to add a keycode to a prev_kcs to remember it.
+When full the last code gets discarded and replaced by
+the new one.
+*/
+void add_to_prev(uint16_t kc){
+  for (int i=0; i<prev_indx; i++){
+    if (kc == prev_kcs[i])
+      return;
+  }
+  if (prev_indx == 6){
+    for (int i=5; i>0; i--){
+      prev_kcs[i] = prev_kcs[i-1];
+    }
+    prev_kcs[0] = kc;
+  } else {
+    prev_kcs[prev_indx] = kc;
+    prev_indx++;
   }
 }
 
-
-// bool thirdRow(uint16_t keycode, keyrecord_t *record) {
-//   bool isLeftShiftPressed = get_mods() & MOD_BIT(KC_LSHIFT);
-//   bool isRightShiftPressed = get_mods() & MOD_BIT(KC_RSHIFT);
-//
-//   switch (keycode) {
-//     // ö to ; and Ö to ;
-//     case KC_COMM:
-//       if (record->event.pressed){
-//         if (isLeftShiftPressed || isRightShiftPressed) {
-//           unregister_code(KC_COMM);
-//           register_code(MY_COL);
-//         } else {
-//           unregister_code(KC_COMM);
-//           shiftIsRegistered = true;
-//           register_code(KC_RSHIFT);
-//           register_code(MY_SEMICOL);
-//         }
-//       } else {
-//         unregister_code(MY_SEMICOL);
-//         unregister_code(KC_COMM);
-//         unregister_code(MY_COL);
-//         if (shiftIsRegistered) {
-//           unregister_code(KC_RSHIFT);
-//           shiftIsRegistered = false;
-//         }
-//       }
-//       return false;
-//
-//     // ä to ' and " to ;
-//     case KC_QUOT:
-//       if (record->event.pressed){
-//         if (isLeftShiftPressed || isRightShiftPressed) {
-//           unregister_code(KC_QUOT);
-//           register_code(KC_2);
-//         } else {
-//           unregister_code(KC_QUOT);
-//           register_code(KC_NONUS_HASH);
-//         }
-//       } else {
-//         unregister_code(KC_QUOT);
-//         unregister_code(KC_NONUS_HASH);
-//         unregister_code(KC_2);
-//       }
-//       return false;
-//
-//     default:
-//       if (altIsRegistered) {
-//         unRegisterAlt();
-//       }
-//       if (shouldReaddShiftOnRelease) {
-//         unregister_code(KC_RSFT);
-//         shouldReaddShiftOnRelease = false;
-//       }
-//       return true;
-//   }
-// }
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-
-  bool val = topRow(keycode, record);
-  //val = thirdRow(keycode, record);
-  return val;
+/*
+Unregisters all codes saved in prev_kcs and resets prev_indx.
+gets called on multiple occasions mainly when shift is released
+and when frankenkeycodes are pressed. Prevents output of
+wrong characters when really specific key combinations
+that would never occur during normal usage are pressed.
+*/
+void unreg_prev(void){
+  if (prev_indx == 0)
+    return;
+  for (int i=0; i<prev_indx; i++){
+    unregister_code(prev_kcs[i]);
+  }
+  prev_indx = 0;
 }
 
+// These indicate if left and right shift are physically pressed
+bool lshift = false;
+bool rshift = false;
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case KC_LSFT:
+      if(record->event.pressed) {
+        unregister_code(KC_LSFT);
+        register_code(KC_LSFT);
+        lshift = true;
+      } else {
+        unreg_prev();
+        if (!rshift)
+          unregister_code(KC_LSFT);
+        lshift = false;
+      }
+      return false;
+    case KC_RSFT:
+      if(record->event.pressed) {
+        unregister_code(KC_LSFT);
+        register_code(KC_LSFT);
+        rshift = true;
+      } else {
+        unreg_prev();
+        if (!lshift)
+          unregister_code(KC_LSFT);
+        rshift = false;
+      }
+      return false;
+    case KC_2:
+      NORM_ALGR(KC_2, KC_2); // Shift 2 to @
+    case KC_4:
+      NORM_ALGR(KC_4, KC_4); // Shift 4 to $
+    case KC_6:
+      NORM_SHIFT(KC_6, KC_RBRC); // Shift 6 to ^
+    case KC_7:
+      NORM_SHIFT(KC_7, KC_6); // Shift 7 to &
+    case KC_8:
+      NORM_SHIFT(KC_8, KC_NONUS_HASH); // Shift 8 to *
+    case KC_9:
+      NORM_SHIFT(KC_9, KC_8); // Shift 9 to (
+    case KC_0:
+      NORM_SHIFT(KC_0, KC_9); // Shift 0 to )
+    case SE_PLUS:
+      NORM_SHIFT(SE_MINS, SE_MINS); // + to - | ? to _
+    case SE_ACUT:
+      NORM_SHIFT(SE_PLUS, KC_0); // ´ to + | ` to =
+    case SE_AA:
+      SHIFT_SHIFT_ALGR(KC_8, KC_8); // å to ( | ˚ to [
+    case SE_QUOT:
+      SHIFT_SHIFT_ALGR(KC_9, KC_9); // ¨ to ) | ^ to ]
+    case SE_OSLH:
+      SHIFT_SHIFT(KC_COMM, KC_DOT); // ö to ; | Ö to :
+    case SE_AE:
+      NORM_SHIFT(KC_NONUS_HASH, KC_2); // ä to ' | Ä to "
+    case KC_NUBS:
+      SHIFT_ALGR_ALGR(KC_7, KC_7); // ' to \ | * to |
+    // TODO: handle <>
+    case KC_COMM:
+      NORM_NORM(KC_COMM, KC_GRV); // ; to <
+    case KC_DOT:
+      NORM_SHIFT(KC_DOT, KC_GRV); // : to >
+    case KC_SLSH:
+      SHIFT_SHIFT(KC_7, KC_MINS); // - to / | _ to ?
 
+    default:
+      if(record->event.pressed) {
+        if (lshift || rshift)
+          register_code(KC_LSFT);
+        else
+          unregister_code(KC_LSFT);
+      }
+      return true;
+  }
 
-
+  return true;
+}
